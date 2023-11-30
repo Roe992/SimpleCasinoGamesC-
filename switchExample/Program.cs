79ffff2b3e19a7eacb5222cd64c2b5658305c63a
Manuel <manugrassi9@gmail.com>
2023-11-30 20:30:02 -0300

    21 working in a simple way and with numbers 1 - 12 instead of actual cards

--- switchExample/Program.cs
+++ switchExample/Program.cs
@@ -2,52 +2,83 @@
 //Console.WriteLine("Hello, World!");
 
 
-int playerTotal = 22;
-int dealerTotal = 25;
+int playerTotal = 0;
+int dealerTotal = 0;
 string message = "";
+int num = 0;
+int numD = 0;
 
-string switchControl = "21"; 
+string switchControl = "menu";
 
 //blackjack
-
-switch (switchControl)
+while (true)
 {
+    switch (switchControl)
+    {
 
-    case "menu":
-        Console.WriteLine( "Welcome to C# A S I N O \n Para Blackjack ingrese 21");
-        break;
+        case "menu":
+            Console.WriteLine("Welcome to C# A S I N O \n Para Blackjack ingrese 21");
+            switchControl = Console.ReadLine();
+            break;
 
-    case "21":
-        Console.WriteLine("Welcome to 21 Blackjack");
-        
+        case "21":
+            Console.WriteLine("Welcome to 21 Blackjack");
+                System.Random random = new System.Random();
+            numD = random.Next(1, 12);
+            dealerTotal += numD;
+            playerTotal = random.Next(1, 12);
+            Console.WriteLine($"your first card is {playerTotal} \n Dealer´s hand: {dealerTotal} \n your total is: {playerTotal}");
+            do
+            {
 
-        if (playerTotal < 22 && dealerTotal < 22 && playerTotal > dealerTotal)
-        {
-            message = $"you beat the dealer, congratulations.\n  player:{playerTotal} dealer: {dealerTotal}";
+                
+                num = random.Next(1, 12);
+                playerTotal = playerTotal + num;
+                Console.WriteLine($"your new card is: {num} \n Dealer´s hand: {dealerTotal} \n your total is: {playerTotal}");
+                if (playerTotal < 21) { Console.WriteLine("do you want to hit or stand"); }
 
-        }
-        else if (playerTotal < 22 && dealerTotal < 22 && playerTotal < dealerTotal)
-        {
-            message = $"the dealer won.\n  player:{playerTotal} dealer: {dealerTotal}";
-        }
-        else if (playerTotal < 22 && dealerTotal > 21)
-        {
-            message = $"the dealer exceeded 21 and you didn't, congratulations you won.\n " +
-                $"you: {playerTotal} dealer: {dealerTotal}";
-        }
-        else if (playerTotal > 21)
-        {
-            message = $"player excedeed 21 \n player:{playerTotal} dealer: {dealerTotal}";
-        }
-        else
-        {
-            message = $"Draw \n player:{playerTotal} dealer: {dealerTotal}";
-        }
 
-    Console.WriteLine(message);
-        break;
+            } while (playerTotal < 21 && Console.ReadLine() == "hit");
 
-    default:
-        Console.WriteLine( "valor no admitido" );
-        break;
+            while (dealerTotal < 17 && playerTotal <= 21)
+            {
+                numD = random.Next(1, 12);
+                dealerTotal += numD;
+                Console.WriteLine($"The dealer draws a card :{numD} \n Dealer´s total : {dealerTotal}");
+            }
+                        
+            if (playerTotal < 22 && dealerTotal < 22 && playerTotal > dealerTotal)
+            {
+                message = $"you beat the dealer, congratulations.\n  player:{playerTotal} dealer: {dealerTotal}";
+
+            }
+            else if (playerTotal < 22 && dealerTotal < 22 && playerTotal < dealerTotal)
+            {
+                message = $"the dealer won.\n  player:{playerTotal} dealer: {dealerTotal}";
+            }
+            else if (playerTotal < 22 && dealerTotal > 21)
+            {
+                message = $"the dealer exceeded 21 and you didn't, congratulations you won.\n " +
+                    $"you: {playerTotal} dealer: {dealerTotal}";
+            }
+            else if (playerTotal > 21)
+            {
+                message = $"player excedeed 21 \n player:{playerTotal} dealer: {dealerTotal}";
+            }
+            else
+            {
+                message = $"Draw \n player:{playerTotal} dealer: {dealerTotal}";
+            }
+
+            Console.WriteLine(message);
+            playerTotal = 0;
+            dealerTotal = 0;
+            switchControl = "menu";
+            break;
+
+        default:
+            Console.WriteLine("valor no admitido");
+            switchControl = "menu";
+            break;
+    }
 }
